5bf56c3f5e35f32df6127e97686e986a8cbe3c86
Jesus Ponce <j.emmanuel.pb@hotmail.com>
2020-06-23 20:57:59 -0500

    Page index and iterations post

---
+++ components/postItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="post">
+    <a class="post-header post-header-link clickable">
+      <h4 class="title is-4">{{ title }}</h4>
+      <h5 class="subtitle is-5">{{ subtitle }}</h5>
+    </a>
+    <div class="post-content">
+      by Filip Jerga, 27th Jan, 2019
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {type: String},
+    subtitle: {type: String}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <navbar />
+    <div class="blogs-page">
+      <div class="main-content">
+        <div class="container">
+          <div class="columns is-mobile">
+            <div class="column is-8 is-offset-2">
+              <div class="section">
+                <div class="title">
+                  <h1>Newest Posts</h1>
+                  <hr>
+                </div>
+                <postItem
+                  v-for="post in posts"
+                  :key="post.id"
+                  :title="post.title"
+                  :subtitle="post.subtitle"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from '~/components/navbar.vue'
+import postItem from '~/components/postItem.vue'
+export default {
+  components: {
+    navbar,
+    postItem
+  },
+  data(){
+    return{
+      posts: [
+        {
+          id: 1,
+          title: 'My first post',
+          subtitle: 'My first subtitle post'
+        },
+        {
+          id: 2,
+          title: 'My second post',
+          subtitle: 'My seconf subtitle post'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .post-content {
+    font-style: italic;
+  }
+  .post {
+    margin-bottom: 20px;
+    padding: 5px;
+    border-bottom: 2px solid transparent;
+  }
+  .post:hover {
+    border-bottom: 2px solid #e8e8e8;
+  }
+</style>
